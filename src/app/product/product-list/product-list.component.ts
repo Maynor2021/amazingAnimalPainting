a0152bf45b86b827e571857b9cc19cb806bab350
Maynor2021 <maynoravila19@gmail.com>
2025-03-29 08:16:49 -0600

    Primer commit subiendo proyecto

---
+++ src/app/product/product-list/product-list.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { ProductService } from '../product.service';
+import { OnInit } from '@angular/core';
+import { Product } from '../../models/product';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MDCRipple} from '@material/ripple';
+import { FlexLayoutModule } from 'ngx-flexible-layout';
+
+
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [CommonModule,MatButtonModule,MatCardModule],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css',
+  
+})
+export class ProductListComponent implements OnInit{
+
+  products:Product[]=[];
+  constructor (private productservice:ProductService){
+
+  }
+  ngOnInit(): void {
+    this.productservice.getProducts().subscribe(data=>{
+      this.products=data;
+    })
+  }
+
+  
+
+}
---
+++ src/app/product/product.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from 'ngx-flexible-layout';
+import { MatCard } from '@angular/material/card';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,FlexLayoutModule,MatCard
+  ]
+})
+export class ProductModule { }
